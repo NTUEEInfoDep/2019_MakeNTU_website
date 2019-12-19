112cd21f36531131eabe0c9b20bdb2c77571a6f8
Brian Chao <whereisbrianchao520@gmail.com>
2019-12-19 11:05:39 +0800

    Updates

---
+++ precache-manifest.1e7da6f5a2a8ef33ba06c7cb8c0f745f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "6b205f8345fcae7ca569",
+    "url": "./static/js/main.9885eaa2.chunk.js"
+  },
+  {
+    "revision": "52c89a7d782a751fc29d",
+    "url": "./static/js/2.8c41bbcd.chunk.js"
+  },
+  {
+    "revision": "6b205f8345fcae7ca569",
+    "url": "./static/css/main.6abf0a59.chunk.css"
+  },
+  {
+    "revision": "52c89a7d782a751fc29d",
+    "url": "./static/css/2.cd4f4180.chunk.css"
+  },
+  {
+    "revision": "c8e9b1668de57f73cfdab667d4b091a7",
+    "url": "./index.html"
+  }
+];
